ed73275c0084c00a1ba1ca5eb43a78d4b833b865
Umang <umangagrawal.rvce@gmail.com>
2019-08-27 19:13:09 +0530

    practise of Array Buffer is completed

--- ArrayBuffer/ArrayBuffer.js
+++ ArrayBuffer/ArrayBuffer.js
@@ -28,3 +28,55 @@ let view_newbuffer = new Uint32Array(buffer1.slice(0,8));
 console.log(view_newbuffer[0]);
 console.log(view_newbuffer);
 
+
+// Typed array functions
+
+let bf1 = new ArrayBuffer(32);
+
+vw1= new Int8Array(bf1);
+vw2 = new Int32Array(bf1);
+vw3 = new Int16Array(bf1);
+
+console.log(vw1.byteLength);
+console.log(vw1.length);
+console.log(vw1.name);
+console.log(vw1.BYTES_PER_ELEMENT);
+console.log(vw2.BYTES_PER_ELEMENT);
+
+vw4 = Int16Array.of(1,2,3,4,5,6,7);
+console.log(vw4);
+vw5 = Int32Array.from([2,3,4,5,6,7]);
+vw6 = Int16Array.from(new Set([2,3,4,5,6]));
+console.log(vw5);
+//console.log(vw6);
+
+vw7 = vw4.entries();
+vw8 = vw4.map((element)=> element*2);
+console.log(vw7,'\n',vw8);
+console.log(vw4.every((element)=>element>0));
+console.log(vw4.some((element)=>element%2==0));
+console.log(vw4.filter((element)=>element%2==0));
+console.log(vw4.reduce((previous,curent)=>previous*curent));
+console.log(vw4.slice(3,5));
+console.log(vw4.subarray(3,5));
+
+console.log(vw1.find((element)=>element%2==0));
+console.log(vw1.findIndex((element)=>element%2==0));
+console.log(vw1.reduceRight((previous,current)=>previous*current));
+console.log(Int16Array[Symbol.species]);
+iter1= vw1.keys();
+iter2 = vw1.values();
+console.log(iter1.next());
+console.log(iter2.next());
+
+vw9 = new Int16Array(bf1,4);
+console.log(vw9.byteOffset);
+
+iter3= vw1[Symbol.iterator]();
+console.log(iter3.next().value);
+
+console.log(vw1.toString());
+console.log(vw1.join('me'));
+
+
+
